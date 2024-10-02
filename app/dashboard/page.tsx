3db9a3e1bfbf8297bd236146b8c64e8ed7d72562
Kameshwar Das <kameshwardas2@gmail.com>
2024-10-03 02:04:10 +0530

    first commit

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,20 @@
 "use client";
-import React, { useState } from 'react'
-import SearchSection from './_components/SearchSection'
-import TemplateListSection from './_components/TemplateListSection'
+import React, { useState } from 'react';
+import SearchSection from './_components/SearchSection';
+import TemplateListSection from './_components/TemplateListSection';
 
-function Page() {
-  const [userSearchInput, setUserSearchInput] = useState<string>('')
+function Page({ onSearchInput }: { onSearchInput: (value: string) => void }) {
+  const [userSearchInput, setUserSearchInput] = useState<string>('');
 
   return (
     <div className='p-3'>
-      <SearchSection onSearchInput={(value: string) => setUserSearchInput(value)} />
+      <SearchSection onSearchInput={(value: string) => {
+        setUserSearchInput(value);
+        onSearchInput(value); // Call the parent handler if necessary
+      }} />
       <TemplateListSection userSearchInput={userSearchInput} />
     </div>
-  )
+  );
 }
 
-export default Page
+export default Page;
