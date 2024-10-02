8f4d14620f27b649a4c3a1d339c5257c2423d931
Kameshwar Das <kameshwardas2@gmail.com>
2024-10-02 15:29:51 +0530

    Update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import TemplateListSection from './_components/TemplateListSection'
 
 
 
-function page({onSearchInput}) {
+function page() {
   const [userSearchInput, setUserSearchInput] = useState<string>()
   return (
     <div className='p-3'>
